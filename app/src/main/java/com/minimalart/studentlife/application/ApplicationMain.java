55e23aa7a51a195bea1d24b7780a4dcd3bdb564f
Gabi <ytgabi98@gmail.com>
2017-02-08 18:29:10 +0200

    added ACRA crash support via EMAIL

---
+++ app/src/main/java/com/minimalart/studentlife/application/ApplicationMain.java
@@ -0,0 +1,9 @@
+package com.minimalart.studentlife.application;
+
+import android.app.Application;
+
+/**
+ * Created by ytgab on 08.02.2017.
+ */
+public class ApplicationMain extends Application {
+}
